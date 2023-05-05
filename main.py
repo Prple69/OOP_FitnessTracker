2e1ce7e2279c0294598a91794b0f64a7e874de3e
Prple69 <prple666@mail.ru>
2023-05-05 16:22:39 +0700

    add classes, constants, basic functions

---
+++ main.py
@@ -0,0 +1,42 @@
+M_IN_KM = 1000
+
+class Training():
+    action: int
+    duration: float
+    weight: float
+
+    def get_distance():
+        pass
+
+    def get_mean_speed():
+        pass
+
+    def get_spent_calories():
+        pass
+
+    def show_training_info():
+        pass
+
+class Running():
+    action: int
+    duration: float
+    weight: float
+
+    LEN_STEP = 0.65
+
+class SportsWalking():
+    action: int
+    duration: float
+    weight: float
+
+    LEN_STEP = 1.38
+
+class Swimming():
+    action: int
+    duration: float
+    weight: float
+
+class InfoMessage():
+    action: int
+    duration: float
+    weight: float
